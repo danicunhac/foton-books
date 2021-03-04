fb36370e775bb04e3cbb4d4bce9a4ddd772e3bd5
Daniel Cunha <danielcc.jp@gmail.com>
2021-03-03 23:55:35 -0300

    feat: books list

--- src/components/BooksList/index.tsx
+++ src/components/BooksList/index.tsx
@@ -1,16 +1,64 @@
-import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import React, { useContext, useEffect, useState } from 'react';
+import { SearchContext } from '../../context/SearchContext';
 
-import { Book, Container } from './styles';
+import {
+  Book,
+  Container,
+  Wrapper,
+  ButtonsContainer,
+  ShowMoreButton,
+  ShowLessButton,
+} from './styles';
 
 const BooksList = ({ books }) => {
+  const { searchName, handleSelect } = useContext(SearchContext);
+  const [shownBooks, setShownBooks] = useState([]);
+  const [visibleAmount, setVisibleAmount] = useState(4);
+
+  useEffect(() => {
+    if (searchName) {
+      const filteredBooks = books.filter((book) =>
+        book.volumeInfo.title.toLowerCase().includes(searchName.toLowerCase())
+      );
+
+      console.log(filteredBooks);
+
+      setShownBooks(filteredBooks);
+      return;
+    }
+
+    setShownBooks(books);
+  }, [searchName]);
+
+  const handleShowMoreBooks = () => {
+    setVisibleAmount((prevAmount) => prevAmount + 4);
+  };
+
+  const handleShowLessBooks = () => {
+    setVisibleAmount((prevAmount) => prevAmount - 4);
+  };
+
   return (
-    <Container>
-      {books.map((book) => (
-        <Book>
-          <img src={book.volumeInfo.imageLinks.thumbnail} key={book.id} />
-        </Book>
-      ))}
-    </Container>
+    <Wrapper>
+      <Container>
+        {shownBooks.slice(0, visibleAmount).map((book) => (
+          <Link key={book.id} href="/books/details">
+            <Book onClick={() => handleSelect(book)}>
+              <img src={book.volumeInfo.imageLinks.thumbnail} />
+            </Book>
+          </Link>
+        ))}
+      </Container>
+      <ButtonsContainer>
+        {visibleAmount > 4 ? (
+          <ShowLessButton onClick={handleShowLessBooks}>
+            Show less
+          </ShowLessButton>
+        ) : null}
+        <ShowMoreButton onClick={handleShowMoreBooks}>Show more</ShowMoreButton>
+      </ButtonsContainer>
+    </Wrapper>
   );
 };
 
--- src/components/BooksList/styles.ts
+++ src/components/BooksList/styles.ts
@@ -1,11 +1,20 @@
 import styled from 'styled-components';
+import { appearFromRight } from '../../styles/Home/styles';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem 0;
+`;
 
 export const Container = styled.div`
-  padding: 2rem 0;
+  padding: 1rem 0;
   flex: 1;
   display: flex;
   flex-wrap: wrap;
-  justify-content: space-around;
+
+  animation: ${appearFromRight} 1s;
 
   div {
     margin-bottom: 1rem;
@@ -15,11 +24,35 @@ export const Container = styled.div`
 export const Book = styled.div`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  margin-left: 2rem;
 
   img {
     object-fit: contain;
-    border-radius: 10px;
     box-shadow: 0 0 16px rgba(0, 0, 0, 0.1);
+    cursor: pointer;
+    height: 300px;
   }
+`;
+
+export const ButtonsContainer = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+
+  button {
+    width: 200px;
+    border-radius: 10px;
+    border: 0;
+    padding: 0.5rem;
+  }
+`;
+
+export const ShowMoreButton = styled.button`
+  background: purple;
+  color: #fff;
+`;
+
+export const ShowLessButton = styled.button`
+  background: #000;
+  color: #fff;
 `;
